5737c4b44a591761032c693023e636989ce4fcd2
Vincent.Chan <vencent@gmail.com>
2022-07-04 17:15:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,10 +18,10 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = 'device_id=213070643316046363862750445cfff4592a8b410a53ec53368c85df9c; r_sort_type=score; __ckguid=q345sTaAYAaxcEit3L45wF2; homepage_sug=b; __jsluid_s=8a1ebde26715237d498df27d65e20a4c; footer_floating_layer=0; ad_date=4; bannerCounter=[{"number":0,"surplus":1},{"number":0,"surplus":1},{"number":0,"surplus":1},{"number":0,"surplus":1},{"number":0,"surplus":1},{"number":0,"surplus":1}]; ad_json_feed={}; sess=AT-m3kyFng3C3yx4z2ecDPi+ppuqQrrhkuRdzQOifG7WbBCIVqcXjadDgFckxN01SCeCDc8wocYImp5E3DA6PBK1d1nDUSZFvFgjDMrD3DbzREpbz7uDfNW4okt; user=user:3439916819|3439916819; smzdm_id=3439916819; _zdmA.uid=ZDMA.C-IGQ6ak5.1656923844.2419200; sensorsdata2015jssdkcross={"distinct_id":"3439916819","first_id":"1759bc8283d478-00b844e074796e-7d677f67-2073600-1759bc8283e294","props":{"$latest_traffic_source_type":"自然搜索流量","$latest_search_keyword":"未取到值","$latest_referrer":"https://www.baidu.com/link","$latest_landing_page":"https://post.smzdm.com/p/ad28p3rp/"},"$device_id":"1759bc8283d478-00b844e074796e-7d677f67-2073600-1759bc8283e294"}; _zdmA.time=1656925754365.3987.https://www.smzdm.com/; smzdm_user_view=86026244659977435C6DB34C43951977; smzdm_user_source=144A871E3C82107C880866460AAC390A'
+TEST_COOKIE = ''
 
 
 """
 调试用 SERVERCHAN_SECRETKEY
 """
-SERVERCHAN_SECRETKEY = 'SCT159046TUyT01y3kXE40pK7fTZiTZBTg'
+SERVERCHAN_SECRETKEY = ''
