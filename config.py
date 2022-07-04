355ba9f520377946c59bf1b4aae9b3bb40847600
Vincent.Chan <vencent@gmail.com>
2022-07-04 17:17:05 +0800

    Update config.py

--- config.py
+++ config.py
@@ -14,12 +14,10 @@ DEFAULT_HEADERS = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
         }
 
-
 """
 调试用 COOKIE
 """
 TEST_COOKIE = ''
-
 
 """
 调试用 SERVERCHAN_SECRETKEY
